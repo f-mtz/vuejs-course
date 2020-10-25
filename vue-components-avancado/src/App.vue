0ef902483d4eb90dea3c98d4a08d8252e286683b
Felipe R da Silva <mtz.dev.si@gmail.com>
2020-10-25 00:48:34 -0300

    Alternando varios Components com Components Dinamicos

--- vue-components-avancado/src/App.vue
+++ vue-components-avancado/src/App.vue
@@ -1,47 +1,29 @@
 <template>
   <div id="app" class="container">
-    <div>
-       <div class="topo"><img src="./imagens/vue-icon.png"></div>
-       <div class=""><h1>Vue JS</h1></div>
-    </div>
+    <h1>Components Dinâmicos</h1>
+    <button @click="componentSelecionado = 'Home'">Home</button>
+    <button @click="componentSelecionado = 'PostsLista'">Posts</button>
+    <button @click="componentSelecionado = 'Sobre'">Sobre</button>
 
-    <h1>Forma padrão:</h1>
-    <PostsLista :posts="objs"/>
-    <!-- AQUI OS COMPONENTS FILHOS SERÃO RENDERIZADOS E ESTILIZADOS DE FORMA CUSTOMIZADA -->
-    <hr>
-
-    <h1>Slost com Escopo:</h1>
-    <PostsLista :posts="objs">
-      <template slot-scope="{ conteudo }">
-        <h2 style="color:blue">{{ conteudo.titulo }}</h2>
-        <p> {{ conteudo.conteudo }} </p>
-        <small> {{conteudo.autor }} </small>
-      </template>
-    </PostsLista>
-
-    <PostsLista :posts="objs">
-      <template slot-scope="slotProps">
-        <h2 style="color:red">{{ slotProps.conteudo.titulo }}</h2>
-        <p> {{ slotProps.conteudo.conteudo }} </p>
-        <small> {{slotProps.conteudo.autor }} </small>
-      </template>
-    </PostsLista>
-  <!-- RESUMO DA AULA:
-
-       O CONCEITO DE SLOTS COM ESCOPO É IMPORTANTE POIS PERMITE POR EXEMPLO IMPLEMENTAR LAYOUTS CUSTOMIZADOS E REUTILIZÁVEIS POPULADOS PELA APLICAÇÃO
-  -->
+    <p> {{ componentSelecionado }} </p>
+    <component :is="componentSelecionado"></component>
   </div>
 </template>
 
 <script>
+import Home from './components/Home.vue'
 import PostsLista from './components/PostsLista.vue'
+import Sobre from './components/Sobre.vue'
 
 export default {
   components: {
-    PostsLista
+    Home,
+    PostsLista,
+    Sobre
   },
   data () {
     return {
+      componentSelecionado: 'Home',
       objs: [
         { id: 1, titulo: 'Components no Vue', conteudo: 'Components são uma das peças mais importantes no Vue', autor: 'Plinio Naves' },
         { id: 2, titulo: 'Distribuindo conteúdos com slots', conteudo: 'Slots podem ser usados como repositórios de código HTML', autor: 'Plinio Naves' }
